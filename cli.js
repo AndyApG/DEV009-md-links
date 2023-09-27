cc7aa79f23ed312be5181de2996c6b24a0180ff0
Andrea <a.aparicio.g@hotmail.com>
2023-09-27 00:39:59 -0600

    se agregan las opciones --validate y --stats a cli

--- cli.js
+++ cli.js
@@ -3,10 +3,30 @@
 const mdLinks = require('./mdLinks');
 
 const path = process.argv[2];
+const options = process.argv.slice(3);
 
-mdLinks(path,true).then((result)=>{
-  console.log(result);
-}).catch((error)=>{
-  console.log(error);
-});
-
+if (options.includes('--validate')) {
+  mdLinks(path, true).then((result) => {
+    if (options.includes('--stats')) {
+      const set = new Set(result);
+      const broken = result.filter((item) => item.ok === 'FAIL').length;
+      console.log(' Total', result.length, '\n', 'Unique:', set.size, '\n', 'Broken:', broken);
+    }
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
+  });
+} else if (options.includes('--stats')) {
+  mdLinks(path).then((result) => {
+    const set = new Set(result);
+    console.log(' Total', result.length, '\n', 'Unique:', set.size);
+  }).catch((error) => {
+    console.log(error);
+  });
+} else {
+  mdLinks(path).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
+  });
+}
