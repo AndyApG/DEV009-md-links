45c82192765d79787c8cdb4f0fa0745a47920cbe
Andrea <a.aparicio.g@hotmail.com>
2023-09-28 11:28:10 -0600

    se arreglaron errores de indentacion, reinstale chalk a una version previa, se agregaron colores a los mensajes de error

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const chalk = require('chalk');
 const mdLinks = require('./mdLinks');
 
 const path = process.argv[2];
@@ -7,26 +7,46 @@ const options = process.argv.slice(3);
 
 if (options.includes('--validate')) {
   mdLinks(path, true).then((result) => {
-    if (options.includes('--stats')) {
-      const set = new Set(result.map((results) => results.href));
-      const broken = result.filter((item) => item.ok === 'FAIL').length;
-      console.log(' Total', result.length, '\n', 'Unique:', set.size, '\n', 'Broken:', broken);
+    if (options.includes('--validate')) {
+      mdLinks(path, true).then((result) => {
+        const set = new Set(result.map((results) => results.href));
+        const broken = result.filter((item) => item.ok === 'FAIL').length;
+        console.log(
+          chalk.hex('#7F00FF').bold('*** VALIDATED STATISTICS ***\n'),
+          chalk.green('\n * Total:'),
+          result.length,
+          '\n',
+          chalk.blue('* Unique:'),
+          set.size,
+          '\n',
+          chalk.hex('#FF0000').bold('* Broken:'),
+          broken,
+        );
+      });
+    } else {
+      console.log(chalk.hex('#7F00FF').bold('*** LINKS FOUND AND VALIDATED ***\n'), result);
     }
-    console.log(result);
   }).catch((error) => {
-    console.log(error);
+    console.log(chalk.hex('#FF0000').bold('* Error :'), error);
   });
 } else if (options.includes('--stats')) {
   mdLinks(path).then((result) => {
     const set = new Set(result.map((results) => results.href));
-    console.log(' Total', result.length, '\n', 'Unique:', set.size);
+    console.log(
+      chalk.hex('#7F00FF').bold('*** STATISTICS ***\n'),
+      chalk.green('\n * Total:'),
+      result.length,
+      '\n',
+      chalk.blue('* Unique:'),
+      set.size,
+    );
   }).catch((error) => {
-    console.log(error);
+    console.log(chalk.hex('#FF0000').bold('* Error :'), error);
   });
 } else {
   mdLinks(path).then((result) => {
-    console.log(result);
+    console.log(chalk.hex('#7F00FF').bold('*** LINKS FOUND ***\n'), result);
   }).catch((error) => {
-    console.log(error);
+    console.log(chalk.hex('#FF0000').bold('* Error :'), error);
   });
 }
